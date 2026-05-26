be0e767848a0ee3e0358473465e98fd1e1b48348
StreetReset <youngapulya@gmail.com>
2026-05-26 15:15:41 +0300

    Update project docs and dependencies

--- build_app.bat
+++ build_app.bat
@@ -3,7 +3,8 @@ chcp 65001 >nul
 cd /d "%~dp0"
 
 echo Собираю приложение в один exe-файл...
-.venv\Scripts\python.exe -m PyInstaller --onefile --name DeepSeekChat main.py
+uv sync
+uv run pyinstaller --onefile --name DeepSeekChat main.py
 
 echo.
 echo Готово. Файл будет здесь:
